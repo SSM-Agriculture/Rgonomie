d0b8cc07106adc8816fff717be34ce8059aec50e
Damien Dotta <damien.dotta@live.fr>
2025-03-31 14:34:54 +0200

    fix: plus de cerise sur lire_table

--- app/Modules/Transverse/lire_table.R
+++ app/Modules/Transverse/lire_table.R
@@ -20,17 +20,10 @@ lire_table <- function(id_onglet){
   # Si on est en mode fichier
   if (input$mode_selection == "fichier"){
     
-    # On lit le fichier sélectionné dans CERISE
-    fic_cerise <- input[[paste0(id_onglet, "_fic_cerise")]]
-    
     # On lit le fichier sélectionné sur le poste local
     fic_local <- input[[paste0(id_onglet, "_fic_local")]]
     
-    # Si un fichier a été sélectionné dans CERISE
-    if (length(fic_cerise) > 0){
-      return("Lecture fichier cerise")
-      
-    } else if (length(fic_local) > 0){
+    if (length(fic_local) > 0){
       # Si un fichier a été sélectionné sur le poste local
       return("Lecture fichier poste local")
       
